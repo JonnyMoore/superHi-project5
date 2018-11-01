704ceb87a3b254c2fd50d62e8b913360e1fa694c
Ben's retina iMac <bendavies@Benss-iMac.local>
2018-11-01 17:44:52 +0000

    update header

---
+++ assets/js/header.js
@@ -0,0 +1,30 @@
+// when I scroll down the page, at acertain point
+// add a class to make the header white
+
+const headerTag = document.querySelector("header")
+
+const toggleHeader = function () {
+    const pixels = window.pageYOffset
+    const alpha = pixels / 1000
+    
+    if (pixels > 60) {
+        headerTag.classList.add("scrolled")
+    } else {
+        headerTag.classList.remove("scrolled")
+    }
+}
+
+const fadeBox = function () {
+    const pixels = window.pageYOffset
+    
+    headerTag.style.boxShadow = `0 0 10px rgba(0,0,0,$(alpha))`
+}
+
+toggleHeader() // runs the function instantly on pageload
+fadeBox()
+
+// runs the function on scroll
+document.addEventListener("scroll", function () {
+    toggleHeader()
+    fadeBox()
+})
